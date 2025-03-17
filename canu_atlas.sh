0d80f914cb12783fd0304a95a0531d1ab6bdc89f
Nathan Wyatt <nathan.a.wyatt@gmail.com>
2025-03-17 14:47:50 -0500

    Update canu_atlas.sh

--- canu_atlas.sh
+++ canu_atlas.sh
@@ -1,1 +1,21 @@
-#running Canu on Atlas
+ #running Canu on Atlas
+
+#Canu is installed as a conda package at the location /project/molecular_epidemiology/conda_envs/canu
+
+#Atlas temp storage
+TMPDIR=/local/scratch/$SLURM_JOB_USER/$SLURM_JOB_ID
+
+
+# Always good practice to reset environment when you start
+ module purge
+
+ # start staging data to the job temporary directory in $TMPDIR
+ MYDIR=`pwd`
+ /bin/cp -r $MYDIR $TMPDIR/
+ cd $TMPDIR
+
+ # add regular job commands like module load
+ # and commands to launch scientific software
+
+ # copy output data off of local scratch
+ /bin/cp -r output $MYDIR/output
